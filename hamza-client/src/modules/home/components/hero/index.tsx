f55eff8efb3a05dec0a73c73f6c113a00bd607f3
Garo <garonazarian09@gmail.com>
2024-03-12 15:26:06 +0700

    Hero Section Template complete

--- hamza-client/src/modules/home/components/hero/index.tsx
+++ hamza-client/src/modules/home/components/hero/index.tsx
@@ -1,33 +1,36 @@
-import { Button, Heading } from "@medusajs/ui"
-import Login from "@/components/AuthenticateAdmin/Login"
-import AuthToken from "@/components/GetAdminToken/AuthToken"
-import Checker from "@/components/CheckCors/Checker"
+import { Box, Flex, Heading, Button, Image } from '@chakra-ui/react';
+import Login from "@/components/AuthenticateAdmin/Login";
+import AuthToken from "@/components/GetAdminToken/AuthToken";
+import Checker from "@/components/CheckCors/Checker";
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Crypto Ecommerce
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Marketplace
-          </Heading>
-        </span>
-
-        {/*<Login />*/}
-        {/*<Checker/>*/}
-        {/*<AuthToken />*/}
-      </div>
-    </div>
+      <Box className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+        <Flex align="center" justify="space-between" className="absolute inset-0 z-10 p-8">
+          <Box className="flex-1">
+            <Heading as="h1" size="xl" className="text-3xl leading-10 text-ui-fg-base font-normal mb-4">
+              Buy & Sell Products Using Crypto as a Community
+            </Heading>
+            <Heading as="h2" size="lg" className="text-xl leading-normal text-ui-fg-subtle font-normal mb-6">
+              By The People, For The People Using Blockchain Tech
+            </Heading>
+            <Button colorScheme="blue" className="text-lg">Connect wallet</Button>
+            {/* Uncomment these components if you need them */}
+            {/*<Login />*/}
+            {/*<Checker/>*/}
+            {/*<AuthToken />*/}
+          </Box>
+          <Box flex="1" className="hidden md:block">
+            <Image
+                src="http://hamza.biz/wp-content/uploads/2024/01/hamzabiz-img.jpg"
+                alt="Image description"
+                className="w-full h-auto object-cover"
+                fallbackSrc="path_to_fallback_image" // In case the original image fails to load
+            />
+          </Box>
+        </Flex>
+      </Box>
   )
 }
 
-export default Hero
+export default Hero;
