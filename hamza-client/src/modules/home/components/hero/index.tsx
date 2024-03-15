3441f639dacaf40467aba4e442101cde1984c4f8
Garo <garonazarian09@gmail.com>
2024-03-15 19:31:45 +0700

    Hero Section Improvements

--- hamza-client/src/modules/home/components/hero/index.tsx
+++ hamza-client/src/modules/home/components/hero/index.tsx
@@ -30,6 +30,7 @@ const Hero = () => {
                     m={6}
                     borderRadius="2xl"
                     maxWidth="1400px"
+                    height="600px"
                     alignItems="center" justifyContent="center"
                     boxShadow="lg"
                     marginLeft={{lg: 4}}
@@ -38,19 +39,19 @@ const Hero = () => {
                     position="relative" // Add this line
                 >
 
-                    <Box flex="1" ml={12}>
-                        <Text fontSize="4xl" color="white" textAlign="left">
+                    <Box flex="1" m={12}>
+                        <Text fontSize="5xl" color="white" textAlign="left">
                             Buy & Sell <br/>
                             Products Using <br/>
                             <Box as="span" fontSize="4xl" fontWeight="bold">Crypto</Box> as a <br/>
                             Community
                         </Text>
-                        <Text color="white" fontSize="xl" m={2}>
+                        <Text color="white" fontSize="2xl" m={2}>
                             By The
                             <Box as="span" fontWeight="bold"> People </Box>
                             <Box as="span" display="inline-flex" alignItems="center"><FaArrowRight
                                 className="ml-1 mr-2"
-                                fontSize={15}/></Box>
+                                fontSize="20px"/></Box>
                             For The<Box as="span" fontWeight="bold"> People</Box>
                             <br/>
                             Using Blockchain Tech
@@ -67,59 +68,59 @@ const Hero = () => {
                     <Box flex="1" className="hidden md:block">
                         <Box position="absolute" top={0} right={0} overflow="hidden">
                             <Image
-                                width={200} // Set the width
-                                height={300} // Set the height
+                                width={368} // Set the width
+                                height={400} // Set the height
                                 src={balloons.src} alt="Image Balloons"/>
                         </Box>
-                        <Box zIndex={2} position="absolute" top={70} right={300} overflow="hidden">
-                            <Image width={200} height={300} src={vr.src} alt="Image description"/>
+                        <Box zIndex={2} position="absolute" top={70} right={340} overflow="hidden">
+                            <Image width={307} height={479} src={vr.src} alt="Image description"/>
                         </Box>
-                        <Box position="absolute" zIndex={1} top={190} right={210} overflow="hidden">
-                            <Image width={110} height={110} src={headphones.src} alt="Image description"/>
+                        <Box position="absolute" zIndex={1} top={300} right={160} overflow="hidden">
+                            <Image width={211} height={256} src={headphones.src} alt="Image description"/>
                         </Box>
-                        <Box position="absolute" zIndex={1} top={180} right={180} overflow="hidden">
+                        <Box position="absolute" zIndex={1} top={180} right={230} overflow="hidden">
                             <Image width={80} height={80} src={coin_3.src} alt="Image description"/>
                         </Box>
-                        <Box position="absolute" zIndex={1} top={250} right={150} overflow="hidden">
+                        <Box position="absolute" zIndex={1} top={250} right={200} overflow="hidden">
                             <Image width={80} height={80} src={coin_2.src} alt="Image description"/>
                         </Box>
-                        <Box position="absolute" zIndex={1} top={55} right={225} overflow="hidden">
+                        <Box position="absolute" zIndex={1} top={55} right={275} overflow="hidden">
                             <Image width={120} height={120} src={coin_4.src} alt="Image description"/>
                         </Box>
-                        <Box position="absolute" top={100} right={440} zIndex={1} overflow="hidden">
+                        <Box position="absolute" top={100} right={570} zIndex={1} overflow="hidden">
                             <Image
-                                width={110} // Set the width
-                                height={110} // Set the height
+                                width={159} // Set the width
+                                height={167} // Set the height
                                 src={swiggle.src} alt="Image Balloons"/>
                         </Box>
-                        <Box position="absolute" top={50} right={450} zIndex={1} overflow="hidden">
+                        <Box position="absolute" top={0} right={580} zIndex={2} overflow="hidden">
                             <Image
-                                width={110} // Set the width
-                                height={110} // Set the height
+                                width={160} // Set the width
+                                height={160} // Set the height
                                 src={coin_5.src} alt="Image Balloons"/>
                         </Box>
-                        <Box position="absolute" top={290} right={440} zIndex={1} overflow="hidden">
+                        <Box position="absolute" top={395} right={520} zIndex={1} overflow="hidden">
                             <Image
-                                width={90} // Set the width
-                                height={140} // Set the height
+                                width={201} // Set the width
+                                height={255} // Set the height
                                 src={twizzler.src} alt="Image Balloons"/>
                         </Box>
-                        <Box position="absolute" top={150} right={530} zIndex={1} overflow="hidden">
+                        <Box position="absolute" top={375} right={690} zIndex={1} overflow="hidden">
                             <Image
-                                width={130} // Set the width
-                                height={130} // Set the height
+                                width={218} // Set the width
+                                height={218} // Set the height
                                 src={coin_1.src} alt="Image Balloons"/>
                         </Box>
-                        <Box position="absolute" top={250} right={520} overflow="hidden">
+                        <Box position="absolute" top={280} right={670} overflow="hidden">
                             <Image
-                                width={50} // Set the width
-                                height={45} // Set the height
+                                width={86} // Set the width
+                                height={118} // Set the height
                                 src={watch.src} alt="Image Balloons"/>
                         </Box>
-                        <Box position="absolute" top={200} right={570} overflow="hidden">
+                        <Box position="absolute" top={200} right={790} overflow="hidden">
                             <Image
-                                width={50} // Set the width
-                                height={45} // Set the height
+                                width={71} // Set the width
+                                height={70} // Set the height
                                 src={coin_6.src} alt="Image Balloons"/>
                         </Box>
 
