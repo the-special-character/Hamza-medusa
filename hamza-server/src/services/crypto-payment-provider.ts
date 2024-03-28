992ff137271e1808fcead2887ff3d20ba35aec4d
Acer <john.kosinski@gmail.com>
2024-03-28 22:55:55 +0700

    Added crypto payment provider for testing

--- hamza-server/src/services/crypto-payment-provider.ts
+++ hamza-server/src/services/crypto-payment-provider.ts
@@ -6,66 +6,101 @@ import {
     PaymentSessionStatus 
 } from "@medusajs/medusa";
 
+/**
+ * @description This is being used as a test right now for payment processing using 
+ * crypto wallets; if used it will be changed alot. Right now only a test.
+ * @author John R. Kosinski
+ */
 class CryptoPaymentService extends AbstractPaymentProcessor {
+    static identifier = "crypto-payment-provider";
+    
+    constructor(container) {
+        super(container);
+    }
+    
     async capturePayment(paymentSessionData: Record<string, unknown>): Promise<PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]> {
+        console.log("CryptoPaymentService: capturePayment");
         return {
-            error: "not implemented",
-            code: ""
+            session_data: paymentSessionData
         };
     }
+    
     async authorizePayment(paymentSessionData: Record<string, unknown>, context: Record<string, unknown>): Promise<PaymentProcessorError | {
         status: PaymentSessionStatus;
         data: PaymentProcessorSessionResponse["session_data"];
-    }>{
+    }> {
+        console.log("CryptoPaymentService: authorizePayment");
         return {
-            error: "not implemented",
-            code: ""
-        };
+            status: PaymentSessionStatus.AUTHORIZED,
+            data: {
+                session_data: paymentSessionData
+            }
+        }
     }
+    
     async cancelPayment(paymentSessionData: Record<string, unknown>): Promise<PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]>
     {
+        console.log("CryptoPaymentService: cancelPayment");
         return {
-            error: "not implemented",
-            code: ""
+            session_data: paymentSessionData
         };
     }
+    
     async initiatePayment(context: PaymentProcessorContext): Promise<PaymentProcessorError | PaymentProcessorSessionResponse> {
+        console.log("CryptoPaymentService: initiatePayment");
         return {
-            error: "not implemented",
-            code: ""
+            session_data: {
+                "method": "initiatePayment",
+                "song": "rondo alla turca", 
+                "craic": 90
+            }
         };
     }
+    
     async deletePayment(paymentSessionData: Record<string, unknown>): Promise<PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]> {
+        console.log("CryptoPaymentService: deletePayment");
         return {
-            error: "not implemented",
-            code: ""
+            session_data: paymentSessionData
         };
     }
+    
     async getPaymentStatus(paymentSessionData: Record<string, unknown>): Promise<PaymentSessionStatus> {
-        return PaymentSessionStatus.ERROR;
+        console.log("CryptoPaymentService: getPaymentStatus");
+        return PaymentSessionStatus.AUTHORIZED;
     }
+    
     async refundPayment(paymentSessionData: Record<string, unknown>, refundAmount: number): Promise<PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]> {
+        console.log("CryptoPaymentService: refundPayment");
         return {
-            error: "not implemented",
-            code: ""
+            session_data: paymentSessionData
         };
     }
+    
     async retrievePayment(paymentSessionData: Record<string, unknown>): Promise<PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]> {
+        console.log("CryptoPaymentService: retrievePayment"); 
         return {
-            error: "not implemented",
-            code: ""
+            session_data: paymentSessionData
         };
     }
+    
     async updatePayment(context: PaymentProcessorContext): Promise<PaymentProcessorError | PaymentProcessorSessionResponse | void> {
+        console.log("CryptoPaymentService: updatePayment");
         return {
-            error: "not implemented",
-            code: ""
+            session_data: {
+                "method": "updatePayment",
+                "time": "leisure time"
+            }
         };
     }
+    
     async updatePaymentData(sessionId: string, data: Record<string, unknown>): Promise<PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]> {
+        console.log("CryptoPaymentService: updatePaymentData");
         return {
-            error: "not implemented",
-            code: ""
+            session_data: {
+                "method": "updatePaymentData",
+                "actor": "stephen chow",
+                "sessionId": sessionId
+            }
         };
     }
 }
