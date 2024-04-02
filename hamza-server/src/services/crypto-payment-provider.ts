d84cba13a335f450af37857f445cda315d3f12fb
Acer <john.kosinski@gmail.com>
2024-04-02 11:46:42 +0700

    Loose connection of custom payment provider to front end

--- hamza-server/src/services/crypto-payment-provider.ts
+++ hamza-server/src/services/crypto-payment-provider.ts
@@ -1,10 +1,13 @@
 import { 
     AbstractPaymentProcessor, 
+    PaymentProvider,
+    PaymentProviderService,
     PaymentProcessorContext, 
     PaymentProcessorError, 
     PaymentProcessorSessionResponse, 
     PaymentSessionStatus 
 } from "@medusajs/medusa";
+import { PaymentIntentOptions } from "medusa-payment-stripe";
 
 /**
  * @description This is being used as a test right now for payment processing using 
@@ -12,14 +15,20 @@ import {
  * @author John R. Kosinski
  */
 class CryptoPaymentService extends AbstractPaymentProcessor {
-    static identifier = "crypto-payment-provider";
+    static identifier = "crypto";
+    static title = "Pay Crypto";
     
-    constructor(container) {
-        super(container);
+    constructor(container, config) {
+        console.log("CryptoPaymentService::container");
+        console.log(container);
+        console.log("CryptoPaymentService::config");
+        console.log(config);
+        super(container, config);
     }
     
     async capturePayment(paymentSessionData: Record<string, unknown>): Promise<PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]> {
         console.log("CryptoPaymentService: capturePayment");
+        console.log(super.container);
         return {
             session_data: paymentSessionData
         };
@@ -45,16 +54,56 @@ class CryptoPaymentService extends AbstractPaymentProcessor {
             session_data: paymentSessionData
         };
     }
+
+    getPaymentIntentOptions(): PaymentIntentOptions {
+        const options: PaymentIntentOptions = {};
+
+        options.capture_method = "manual";
+            
+        options.payment_method_types = [
+            "crypto",
+            "payment",
+            "manual"
+        ]
+        return options;
+    }
     
     async initiatePayment(context: PaymentProcessorContext): Promise<PaymentProcessorError | PaymentProcessorSessionResponse> {
         console.log("CryptoPaymentService: initiatePayment");
+
+        const intentRequestData = this.getPaymentIntentOptions();
+        const { email, currency_code, amount, resource_id, customer } = context;
+
+        const session_data: any = {
+            amount: Math.round(100),
+            currency: "USD",
+            notes: { resource_id },
+            payment: {
+                capture: "manual",
+                capture_options: {
+                    refund_speed: "normal",
+                    automatic_expiry_period: 5,
+                    manual_expiry_period: 10,
+                },
+            },
+            ...intentRequestData,
+        };
+        return {
+            session_data: session_data as any
+        };
+        /*
         return {
             session_data: {
                 "method": "initiatePayment",
                 "song": "rondo alla turca", 
-                "craic": 90
+                "craic": 90, 
+                "payment_methods": [
+                    "one",
+                    "two"
+                ]
             }
         };
+        */
     }
     
     async deletePayment(paymentSessionData: Record<string, unknown>): Promise<PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]> {
@@ -85,12 +134,7 @@ class CryptoPaymentService extends AbstractPaymentProcessor {
     
     async updatePayment(context: PaymentProcessorContext): Promise<PaymentProcessorError | PaymentProcessorSessionResponse | void> {
         console.log("CryptoPaymentService: updatePayment");
-        return {
-            session_data: {
-                "method": "updatePayment",
-                "time": "leisure time"
-            }
-        };
+        return this.initiatePayment(context);
     }
     
     async updatePaymentData(sessionId: string, data: Record<string, unknown>): Promise<PaymentProcessorError | PaymentProcessorSessionResponse["session_data"]> {
