9bb094567a60a9c4ee73726537c4fe63b68c371e
Garo <garonazarian09@gmail.com>
2024-04-05 14:49:54 +0700

    uncommented recommendedItems Component

--- hamza-client/src/app/[countryCode]/(main)/page.tsx
+++ hamza-client/src/app/[countryCode]/(main)/page.tsx
@@ -1,34 +1,34 @@
-import { Product } from '@medusajs/medusa'
-import { Metadata } from 'next'
-import { getCollectionsList, getProductsList } from '@lib/data'
-import FeaturedProducts from '@modules/home/components/featured-products'
-import Hero from '@modules/home/components/hero'
-import RecommendedItems from '@modules/home/components/products'
-import ToggleSection from '@modules/home/components/toggle-section'
-import Testimonials from '@modules/home/components/testimonials'
-import FAQSection from '@modules/home/components/FAQSection'
-import Reputation from '@modules/home/components/reputation'
-import { getRegion } from 'app/actions'
-import { ProductCollectionWithPreviews } from 'types/global'
-import ContactSection from '@modules/home/components/contact-section'
+import { Product } from '@medusajs/medusa';
+import { Metadata } from 'next';
+import { getCollectionsList, getProductsList } from '@lib/data';
+import FeaturedProducts from '@modules/home/components/featured-products';
+import Hero from '@modules/home/components/hero';
+import RecommendedItems from '@modules/home/components/products';
+import ToggleSection from '@modules/home/components/toggle-section';
+import Testimonials from '@modules/home/components/testimonials';
+import FAQSection from '@modules/home/components/FAQSection';
+import Reputation from '@modules/home/components/reputation';
+import { getRegion } from 'app/actions';
+import { ProductCollectionWithPreviews } from 'types/global';
+import ContactSection from '@modules/home/components/contact-section';
 export const metadata: Metadata = {
     title: 'Hamza Shop',
     description:
         'A performant frontend ecommerce starter template with Next.js 14 and Medusa.',
-}
+};
 
 const getCollectionsWithProducts = async (
     countryCode: string
 ): Promise<ProductCollectionWithPreviews[] | null> => {
     const { collections } = await getCollectionsList(0, 3).then(
         (collections) => collections
-    )
+    );
 
     if (!collections) {
-        return null
+        return null;
     }
 
-    const collectionIds = collections.map((collection) => collection.id)
+    const collectionIds = collections.map((collection) => collection.id);
 
     await Promise.all(
         collectionIds.map((id) =>
@@ -39,42 +39,42 @@ const getCollectionsWithProducts = async (
         )
     ).then((responses) =>
         responses.forEach(({ response, queryParams }) => {
-            let collection
+            let collection;
 
             if (collections) {
                 collection = collections.find(
                     (collection) =>
                         collection.id === queryParams?.collection_id?.[0]
-                )
+                );
             }
 
             if (!collection) {
-                return
+                return;
             }
 
-            collection.products = response.products as unknown as Product[]
+            collection.products = response.products as unknown as Product[];
         })
-    )
+    );
 
-    return collections as unknown as ProductCollectionWithPreviews[]
-}
+    return collections as unknown as ProductCollectionWithPreviews[];
+};
 
 export default async function Home({
     params: { countryCode },
 }: {
-    params: { countryCode: string }
+    params: { countryCode: string };
 }) {
-    const collections = await getCollectionsWithProducts(countryCode)
-    const region = await getRegion(countryCode)
+    const collections = await getCollectionsWithProducts(countryCode);
+    const region = await getRegion(countryCode);
 
     if (!collections || !region) {
-        return null
+        return null;
     }
 
     return (
         <>
             <Hero />
-            {/*<RecommendedItems />*/}
+            <RecommendedItems />
             <ToggleSection />
             <Testimonials />
             <Reputation />
@@ -86,5 +86,5 @@ export default async function Home({
             {/*  </ul>*/}
             {/*</div>*/}
         </>
-    )
+    );
 }
