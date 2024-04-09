848af9184c964cc5140962de1c3953dea6518861
Garo <garonazarian09@gmail.com>
2024-04-09 09:40:12 +0700

    Refactor routes, added user creation to seed-onboarding for rapid testing

--- hamza-server/src/api/index.ts
+++ hamza-server/src/api/index.ts
@@ -3,18 +3,12 @@
 
 import { registerOverriddenValidators } from "@medusajs/medusa";
 import { StorePostAuthReq as MedusaStorePostAuthReq } from "@medusajs/medusa/dist/api/routes/store/auth";
-import { AdminPostProductsReq as MedusaAdminPostProductsReq } from "@medusajs/medusa/dist/api/routes/admin/products/create-product";
 import { IsEnum, IsString } from "class-validator";
-
+import { AdminPostProductsReq } from "./routes/products/create-product";
 export class StorePostAuthReq extends MedusaStorePostAuthReq {
   @IsString()
   wallet_address: string;
 }
 
-export class AdminPostProductsReq extends MedusaAdminPostProductsReq {
-  @IsString()
-  store_id: string;
-}
-
-// registerOverriddenValidators(StorePostAuthReq);
+registerOverriddenValidators(StorePostAuthReq);
 registerOverriddenValidators(AdminPostProductsReq);
