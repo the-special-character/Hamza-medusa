00b146fd17012f5e9f97a70a3ae05ffba20922ae
Garo <garonazarian09@gmail.com>
2024-04-15 15:15:09 +0700

    Owner_id added to store

--- hamza-server/src/api/admin/custom/user/route.ts
+++ hamza-server/src/api/admin/custom/user/route.ts
@@ -1,44 +1,45 @@
-import { MedusaRequest, MedusaResponse } from "@medusajs/medusa";
+import { MedusaRequest, MedusaResponse } from '@medusajs/medusa';
 
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
-  const userService = req.scope.resolve("userService");
-  const storeService = req.scope.resolve("storeService");
+    const userService = req.scope.resolve('userService');
+    const storeService = req.scope.resolve('storeService');
 
-  try {
-    const user0 = await userService.create(
-      {
-        email: "GoblinVendor@hamza.com",
-        first_name: "Goblin",
-        last_name: "Vendor",
-      },
-      "password",
-    );
+    try {
+        const user0 = await userService.create(
+            {
+                email: 'GoblinVendor@hamza.com',
+                first_name: 'Goblin',
+                last_name: 'Vendor',
+            },
+            'password'
+        );
 
-    const user1 = await userService.create(
-      {
-        email: "QualityVendor@hamza.com",
-        first_name: "Quality",
-        last_name: "Vendor",
-      },
-      "password",
-    );
+        const user1 = await userService.create(
+            {
+                email: 'QualityVendor@hamza.com',
+                first_name: 'Quality',
+                last_name: 'Vendor',
+            },
+            'password'
+        );
 
-    const user2 = await userService.create(
-      {
-        email: "HeadphonesVendor@hamza.com",
-        first_name: "Headphones",
-        last_name: "Vendor",
-      },
-      "password",
-    );
+        const user2 = await userService.create(
+            {
+                email: 'HeadphonesVendor@hamza.com',
+                first_name: 'Headphones',
+                last_name: 'Vendor',
+            },
+            'password'
+        );
 
-    const store0 = await storeService.addUser(user0);
+        const store0 = await storeService.createStore(user0);
+        const store1 = await storeService.createStore(user1);
 
-    return res.json({ user0, user1, user2, store0 });
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(500)
-      .json({ message: "Internal server error", error: error.message });
-  }
+        return res.json({ user0, user1, user2, store0, store1 });
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(500)
+            .json({ message: 'Internal server error', error: error.message });
+    }
 };
