7e7e1ab60b9f94cde4f648a373b5eb082809869a
Acer <john.kosinski@gmail.com>
2024-04-16 15:02:58 +0700

    fixed error in call-api script

--- hamza-server/scripts/call-api.js
+++ hamza-server/scripts/call-api.js
@@ -25,15 +25,21 @@ async function main()
             body: JSON.stringify({ email: 'admin@medusa-test.com', password:'supersecret' }),
             headers: { 'Content-type': 'application/json; charset=UTF-8' },
         }); 
+        const authData = await authResponse.json();
         
         const response = await fetch('http://localhost:9000/admin/custom/user?email=goblinvendor@hamza.com&password=password', {
             method: 'GET',
-            headers: { 'Cookie': 'connect.sid=s%3AWa1xUWUBc_6S8ZmJxcLDmSXXXVCOGbdU.Rfmyt4%2BSWR%2Bw7WGnBSESFhgoPvo6kGVPph03%2BOI0Wpw', 'Content-type': 'application/json; charset=UTF-8' },
+            headers: { 'Cookie': 'connect.sid=s%3AENcK-STil6VcoXHGJ_BfJ_li0E96O7r7.Hu%2FI58TPs20jB%2BeX6KSjoJ0z6NninxhRd%2B0AK5%2BWEFI', 'Content-type': 'application/json; charset=UTF-8' },
         });
         
-        const data = response.json();
-        const storeIds = [data.store0.id];
-        replaceStoreIdsInSeed("data/seed-1.json", storeIds);
+        const data = await response.json();
+        if (!data.store0) {
+            console.error(data);
+        }
+        else {
+            const storeIds = [data.store0.id];
+            replaceStoreIdsInSeed("data/seed-1.json", storeIds);
+        }
     }
     catch(e) {
         console.error(e);
