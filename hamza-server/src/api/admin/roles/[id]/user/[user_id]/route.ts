f1761ca7656b465e1c343a028efc8f3f94c527bb
Garo <garonazarian09@gmail.com>
2024-04-19 22:01:10 +0700

    TODO added

--- hamza-server/src/api/admin/roles/[id]/user/[user_id]/route.ts
+++ hamza-server/src/api/admin/roles/[id]/user/[user_id]/route.ts
@@ -1,6 +1,8 @@
 import type { MedusaRequest, MedusaResponse } from '@medusajs/medusa';
 import RoleService from '../../../../../../services/role';
 
+// TODO: We can move this to our service layer to keep our route clean,
+//  we already have a service layer to create users this can easily be added there
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
     // omitting validation for simplicity purposes
     const { id, user_id } = req.params;
