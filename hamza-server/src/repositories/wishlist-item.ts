a2cb6d18fe2e016049a9d8891b855fd74e70757e
Garo <garonazarian09@gmail.com>
2024-04-19 15:36:28 +0700

    WishList Service (WIP)

--- hamza-server/src/repositories/wishlist-item.ts
+++
@@ -1,9 +0,0 @@
-import { WishlistItem } from '../models/wishlist-item';
-import { dataSource } from '@medusajs/medusa/dist/loaders/database';
-import { Repository } from 'typeorm';
-
-export const WishlistItemRepository = dataSource
-    .getRepository(WishlistItem)
-    .extend({
-        ...Object.assign(Repository, { target: WishlistItem }),
-    });
--- hamza-server/src/repositories/wishlist.ts
+++
@@ -1,7 +0,0 @@
-import { Wishlist } from '../models/wishlist';
-import { dataSource } from '@medusajs/medusa/dist/loaders/database';
-import { Repository } from 'typeorm';
-
-export const WishlistRepository = dataSource.getRepository(Wishlist).extend({
-    ...Object.assign(Repository, { target: Wishlist }),
-});
