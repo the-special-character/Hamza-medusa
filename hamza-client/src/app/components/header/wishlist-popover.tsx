677a2d2c35e6ca6a8a250a7f5c5cc2baa96dd66e
Garo <garonazarian09@gmail.com>
2024-04-24 18:05:10 +0700

    Wishlist Page (WIP)

--- hamza-client/src/app/components/header/wishlist-popover.tsx
+++ hamza-client/src/app/components/header/wishlist-popover.tsx
@@ -2,7 +2,7 @@
 
 import LocalizedClientLink from '@modules/common/components/localized-client-link';
 import React from 'react';
-import WishlistIcon from '../../../icons/wishlist';
+import WishlistIcon from '@/components/header/wishlist';
 import WishlistPopoverItem from './wishlist-popover-item';
 import useWishlistStore from '@store/wishlist/wishlist-store';
 import { Popover, Transition } from '@headlessui/react';
@@ -14,7 +14,7 @@ const WishlistPopover = () => {
     const iconStyle = { className: 'mr-1' };
 
     return (
-        <div className="relative inline-block text-left mr-2">
+        <Popover className="relative inline-block text-left mr-2">
             <div>
                 <Popover.Button className="inline-flex items-center justify-center w-full rounded p-2 text-sm font-medium hover:opacity-1/2">
                     <WishlistIcon {...iconStyle} />
@@ -22,7 +22,7 @@ const WishlistPopover = () => {
                 </Popover.Button>
             </div>
 
-            <Popover.Items className="origin-top-right absolute right-0 mt-2 w-96 px-6 py-4 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Popover.Button className="origin-top-right absolute right-0 mt-2 w-96 px-6 py-4 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div className="py-1">
                     {wishlist.items.length < 1 ? (
                         <div className="flex justify-center">
@@ -32,30 +32,30 @@ const WishlistPopover = () => {
                         <>
                             {wishlist.items.map((item) => (
                                 <div className="py-2 first:pt-0" key={item.id}>
-                                    <Popover.Item>
+                                    <Popover.Button>
                                         {({ active }) => (
                                             <WishlistPopoverItem
                                                 item={item.product}
                                                 currencyCode="usd"
                                             />
                                         )}
-                                    </Popover.Item>
+                                    </Popover.Button>
                                 </div>
                             ))}
                             <div className="flex flex-col mt-4">
-                                <Popover.Item>
+                                <Popover.Button>
                                     <LocalizedClientLink href="/wishlist">
                                         <button className="text-ui-dark py-2 text-sm w-full border px-3 py-1.5 rounded hover:text-black hover:bg-gray-100">
                                             View Wish List
                                         </button>
                                     </LocalizedClientLink>
-                                </Popover.Item>
+                                </Popover.Button>
                             </div>
                         </>
                     )}
                 </div>
-            </Popover.Items>
-        </div>
+            </Popover.Button>
+        </Popover>
     );
 };
 
