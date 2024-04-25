16b6d7064563b2965ba00fa429aa29cbd7b7f1d7
Karan Chugh <karanchugh02@gmail.com>
2024-04-25 12:15:19 +0000

    ci: store background color

--- hamza-client/src/modules/store/templates/index.tsx
+++ hamza-client/src/modules/store/templates/index.tsx
@@ -1,30 +1,30 @@
-import { Suspense } from 'react'
+import { Suspense } from 'react';
 
-import SkeletonProductGrid from '@modules/skeletons/templates/skeleton-product-grid'
-import RefinementList from '@modules/store/components/refinement-list'
+import SkeletonProductGrid from '@modules/skeletons/templates/skeleton-product-grid';
+import RefinementList from '@modules/store/components/refinement-list';
 // Removed SortOptions import since it's no longer used here
 // import { SortOptions } from "@modules/store/components/refinement-list/sort-products";
 
-import PaginatedProducts from './paginated-products'
+import PaginatedProducts from './paginated-products';
 
 const StoreTemplate = ({
     // Removed sortBy from the props since we won't be using it
     page,
     countryCode,
 }: {
-    page?: string
-    countryCode: string
+    page?: string;
+    countryCode: string;
 }) => {
-    const pageNumber = page ? parseInt(page, 10) : 1
+    const pageNumber = page ? parseInt(page, 10) : 1;
 
     return (
-        <div className="flex flex-col small:flex-row small:items-start py-6 content-container">
+        <div className="flex flex-col small:flex-row small:items-start py-6 content-container !bg-[var(--chakra-colors-black)]">
             {/* If RefinementList component is still necessary for other filters, keep it. Otherwise, consider removing or adjusting it as well. */}
             {/* Since we're not using sortBy, we might not need to pass it to RefinementList */}
             {/*<RefinementList />*/}
             <div className="w-full">
                 <div className="mb-8 text-2xl-semi">
-                    <h1>All products</h1>
+                    <h1 className="!text-white">All products</h1>
                 </div>
                 <Suspense fallback={<SkeletonProductGrid />}>
                     <PaginatedProducts
@@ -35,7 +35,7 @@ const StoreTemplate = ({
                 </Suspense>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default StoreTemplate
+export default StoreTemplate;
