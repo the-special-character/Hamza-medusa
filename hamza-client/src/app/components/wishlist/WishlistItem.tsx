b50347ceb17d1f74576027b249c6994630324693
Garo <garonazarian09@gmail.com>
2024-04-27 14:10:20 +0700

    Small Fixed && TODO

--- hamza-client/src/app/components/wishlist/WishlistItem.tsx
+++ hamza-client/src/app/components/wishlist/WishlistItem.tsx
@@ -16,23 +16,24 @@ const WishlistItem = ({ item }) => {
         console.log('item.product', item);
         removeWishlistItemMutation.mutate(item);
     };
+    // TODO: Add ternary operator if item is null
     return (
         <div className="flex mb-6 last:mb-0">
             <div className="bg-ui rounded-md overflow-hidden mr-4 max-w-1/4">
                 <Image
                     className="h-auto w-full object-cover"
-                    src={item.thumbnail}
+                    src={item?.thumbnail}
                     width="300"
                     height="200"
-                    alt={item.title}
+                    alt={item?.title}
                 />
             </div>
             <div className="flex text-sm flex-grow py-2 justify-between space-x-8">
-                <LocalizedClientLink href={item.handle} className="w-full">
+                <LocalizedClientLink href={item?.handle} className="w-full">
                     <div className="flex flex-col justify-between w-full hover:text-green-400">
                         <div className="flex flex-col">
-                            <p className="font-semibold mb-4">{item.title}</p>
-                            <p>{item.description}</p>
+                            <p className="font-semibold mb-4">{item?.title}</p>
+                            <p>{item?.description}</p>
                         </div>
                     </div>
                 </LocalizedClientLink>
