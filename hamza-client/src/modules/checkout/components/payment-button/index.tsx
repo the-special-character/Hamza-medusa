c08f2de2107ba9f8640f2e8a7d40317cc425da20
WillyDallas <willy.dallas@pm.me>
2024-05-02 19:12:31 +0200

    added comments to the payment button thinking about how to pass reciever_address

--- hamza-client/src/modules/checkout/components/payment-button/index.tsx
+++ hamza-client/src/modules/checkout/components/payment-button/index.tsx
@@ -142,9 +142,9 @@ const CryptoPaymentButton = ({
             console.log(output);
             return {
                 transaction_id: output.transaction_id,
-                payer_address: 'temp',
-                receiver_address: 'temp',
-                escrow_contract_address: 'temp',
+                payer_address: output.receipt.from,
+                receiver_address: 'tbd', // assume this needs to be the array of switchInput[i].payments[j].reciever
+                escrow_contract_address: output.receipt.to,
             };
         } catch (e) {
             console.error('error has occured during transaction', e);
