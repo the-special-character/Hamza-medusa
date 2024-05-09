14328d95d2484e01e76bacb1c94c90fcec7c787f
Acer <john.kosinski@gmail.com>
2024-05-09 18:38:40 +0700

    fixed bug in readRequestBody

--- hamza-server/src/utils/request-body.ts
+++ hamza-server/src/utils/request-body.ts
@@ -1,6 +1,7 @@
 export function readRequestBody(body: any, fieldNames: string[]): any {
     const output = {};
-    for (const name in fieldNames) {
+    body = JSON.parse(JSON.stringify(body));
+    for (const name of fieldNames) {
         output[name] = body[name];
     }
     return output;
