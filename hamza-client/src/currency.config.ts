af85bb19de73cc641a2312eb1835c377acf18358
Acer <john.kosinski@gmail.com>
2024-05-13 12:41:34 +0700

    fixed bug in price format: typo

--- hamza-client/src/currency.config.ts
+++ hamza-client/src/currency.config.ts
@@ -53,7 +53,7 @@ const chainConfig: any = {
         chain_name: 'mainnet',
         usdc: {
             contract_address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-            pprecision: {
+            precision: {
                 db: 2,
                 native: 6,
             },
