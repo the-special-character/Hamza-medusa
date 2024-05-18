db5aa60d9f3392464bb7f6b02615c44491a7758e
Garo <garonazarian09@gmail.com>
2024-05-18 18:14:52 +0700

    Item details

--- hamza-client/src/modules/order/components/items/index.tsx
+++ hamza-client/src/modules/order/components/items/index.tsx
@@ -4,36 +4,49 @@ import { Table } from '@medusajs/ui';
 import Divider from '@modules/common/components/divider';
 import Item from '@modules/order/components/item';
 import SkeletonLineItem from '@modules/skeletons/components/skeleton-line-item';
+import Thumbnail from '@modules/products/components/thumbnail';
 
 type ItemsProps = {
-    items: LineItem[];
-    region: Region;
+    items: any;
 };
 
-const Items = ({ items, region }: ItemsProps) => {
+const Items = ({ items }: ItemsProps) => {
     console.log(`Order Items are ${JSON.stringify(items)}`);
+    console.log(`Order Items are ${items}`);
     return (
         <div className="flex flex-col">
             <Divider className="!mb-0" />
             <Table>
                 <Table.Body>
-                    {items?.length && region
-                        ? items
-                              .sort((a, b) => {
-                                  return a.created_at > b.created_at ? -1 : 1;
-                              })
-                              .map((item) => {
-                                  return (
-                                      <Item
-                                          key={item.id}
-                                          item={item}
-                                          region={region}
-                                      />
-                                  );
-                              })
-                        : Array.from(Array(5).keys()).map((i) => {
-                              return <SkeletonLineItem key={i} />;
-                          })}
+                    {Object.entries(items).map(([id, cartItems]) =>
+                        cartItems.map((item) => (
+                            <Table.Row key={item.id}>
+                                <Table.Cell>
+                                    <div className="flex items-center">
+                                        <p className="text-sm font-semibold">
+                                            Item ID: {item.id}
+                                        </p>
+                                    </div>
+                                </Table.Cell>
+                                <Table.Cell>
+                                    <div className="flex items-center">
+                                        <p className="text-sm font-semibold">
+                                            Quantity: {item.quantity}
+                                            {/* If you need item quantity, use {item.quantity} */}
+                                        </p>
+                                    </div>
+                                </Table.Cell>
+                                <Table.Cell>
+                                    <Thumbnail
+                                        thumbnail={item.thumbnail}
+                                        images={[]}
+                                        height="60px"
+                                    />
+                                    {item.title}
+                                </Table.Cell>
+                            </Table.Row>
+                        ))
+                    )}
                 </Table.Body>
             </Table>
         </div>
--- hamza-client/src/modules/order/templates/order-details-template.tsx
+++ hamza-client/src/modules/order/templates/order-details-template.tsx
@@ -2,7 +2,7 @@
 
 import { Order } from '@medusajs/medusa';
 import { XMark } from '@medusajs/icons';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Help from '@modules/order/components/help';
 import Items from '@modules/order/components/items';
@@ -10,14 +10,82 @@ import OrderDetails from '@modules/order/components/order-details';
 import OrderSummary from '@modules/order/components/order-summary';
 import ShippingDetails from '@modules/order/components/shipping-details';
 import LocalizedClientLink from '@modules/common/components/localized-client-link';
+import axios from 'axios';
 
 type OrderDetailsTemplateProps = {
     order: Order;
 };
 
+interface DetailedOrder extends Order {
+    details?: any; // Further specify if you have the structure of the details
+}
+
+interface Item {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    cart_id: string;
+    order_id: string | null;
+    swap_id: string | null;
+    claim_order_id: string | null;
+    original_item_id: string | null;
+    order_edit_id: string | null;
+    title: string;
+    description: string;
+    thumbnail: string;
+    is_return: boolean;
+    is_giftcard: boolean;
+    should_merge: boolean;
+    allow_discounts: boolean;
+    has_shipping: boolean;
+    unit_price: number;
+    variant_id: string;
+    quantity: number;
+    fulfilled_quantity: number | null;
+    returned_quantity: number | null;
+    shipped_quantity: number | null;
+    metadata: any;
+    currency_code: string;
+}
+
 const OrderDetailsTemplate: React.FC<OrderDetailsTemplateProps> = ({
     order,
 }) => {
+    //
+    const [detailedOrders, setDetailedOrders] = useState<DetailedOrder[]>([]);
+
+    console.log('Orders: ', order.cart_id);
+
+    useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                const { data } = await axios.post(
+                    'http://localhost:9000/custom/order',
+                    {
+                        cart_id: order.cart_id,
+                    }
+                );
+                console.log('Data: ', data);
+                setDetailedOrders(data.order);
+            } catch (error) {
+                console.error('Error fetching orders: ', error);
+            }
+        };
+
+        fetchOrders();
+    }, [order]);
+
+    const specificCart = detailedOrders.reduce((acc, item) => {
+        // Check if the item's cart_id matches the order.cart_id
+        if (item.cart_id === order.cart_id) {
+            if (!acc[item.cart_id]) {
+                acc[item.cart_id] = [];
+            }
+            acc[item.cart_id].push(item);
+        }
+        return acc;
+    }, {});
+
     return (
         <div className="flex flex-col justify-center gap-y-4 ">
             <div className="flex gap-2 justify-between items-center">
@@ -31,7 +99,7 @@ const OrderDetailsTemplate: React.FC<OrderDetailsTemplateProps> = ({
             </div>
             <div className="flex flex-col gap-4 h-full bg-white w-full p-8">
                 <OrderDetails order={order} showStatus />
-                <Items items={order.items} region={order.region} />
+                <Items items={specificCart} />
                 <ShippingDetails order={order} />
                 <OrderSummary order={order} />
                 <Help />
